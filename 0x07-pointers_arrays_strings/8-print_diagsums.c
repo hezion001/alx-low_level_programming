8ee6f7101dd8b8e8187f636f5a3d2d125f3655d4
hezion001 <ibok.hezekiah@gmail.com>
2022-09-26 22:29:11 +0100

    Task 7 modified

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,11 +5,11 @@
  * @size: size of a
  * Return: void
  */
- void print_diagsums(int *a, int size)
- {
-	 int i = 0, x_sum = 0, y_sum = 0;
+void print_diagsums(int *a, int size)
+{
+	int i = 0, x_sum = 0, y_sum = 0;
 
-	 while (i < size)
-		 x_sum += a[size - i][size - i], y_sum += a[i][size - i];
-	 printf("%d, %d", x_sum, y_sum);
- }
+	while (i < size)
+		x_sum += a[size - i][size - i], y_sum += a[i][size - i];
+	printf("%d, %d", x_sum, y_sum);
+}
