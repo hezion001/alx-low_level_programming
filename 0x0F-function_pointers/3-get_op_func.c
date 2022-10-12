635c69c759b234f03f02ae23d90a3b08d6133bc6
hezion001 <ibok.hezekiah@gmail.com>
2022-10-12 21:38:08 +0100

    Task advanced completed

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+/**
+ * get_op_func - gets the operation specified by `s`
+ * @s: a character that represents the mathematical operation
+  * Return: the operation's function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = -1;
+	while (ops[++i].op != NULL)
+		if (*ops[i].op == *s && s[1] == '\0')
+			return (ops[i].f);
+	return (NULL);
+}
